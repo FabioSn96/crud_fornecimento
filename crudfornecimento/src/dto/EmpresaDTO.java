75b47aa0268066572a947abebae9e9362cedb24d
Fabio Nascimento <63240489+FabioSn96@users.noreply.github.com>
2023-03-30 14:57:08 -0300

    Add files via upload

---
+++ crudfornecimento/src/dto/EmpresaDTO.java
@@ -0,0 +1,56 @@
+package dto;
+
+public class EmpresaDTO {
+
+    private String cnpj_empresa, nome_empresa, cep_empresa, nome_fornecedor;
+    private int id_empresa, id_fornecedor;
+
+    public int getId_empresa() {
+        return id_empresa;
+    }
+
+    public void setId_empresa(int id_empresa) {
+        this.id_empresa = id_empresa;
+    }
+
+    public String getCnpj_empresa() {
+        return cnpj_empresa;
+    }
+
+    public void setCnpj_empresa(String cnpj_empresa) {
+        this.cnpj_empresa = cnpj_empresa;
+    }
+
+    public String getNome_empresa() {
+        return nome_empresa;
+    }
+
+    public void setNome_empresa(String nome_empresa) {
+        this.nome_empresa = nome_empresa;
+    }
+
+    public String getCep_empresa() {
+        return cep_empresa;
+    }
+
+    public void setCep_empresa(String cep_empresa) {
+        this.cep_empresa = cep_empresa;
+    }
+
+    public int getId_fornecedor() {
+        return id_fornecedor;
+    }
+
+    public void setId_fornecedor(int id_fornecedor) {
+        this.id_fornecedor = id_fornecedor;
+    }
+
+    public String getNome_fornecedor() {
+        return nome_fornecedor;
+    }
+
+    public void setNome_fornecedor(String nome_fornecedor) {
+        this.nome_fornecedor = nome_fornecedor;
+    }
+
+}
---
+++ crudfornecimento/src/dto/FornecedorDTO.java
@@ -0,0 +1,48 @@
+package dto;
+
+public class FornecedorDTO {
+
+    private String cnpj_fornecedor, nome_fornecedor, cep_fornecedor, email_fornecedor;
+    private int id_fornecedor;
+
+    public int getId_fornecedor() {
+        return id_fornecedor;
+    }
+
+    public void setId_fornecedor(int id_fornecedor) {
+        this.id_fornecedor = id_fornecedor;
+    }
+
+    public String getCnpj_fornecedor() {
+        return cnpj_fornecedor;
+    }
+
+    public void setCnpj_fornecedor(String cnpj_fornecedor) {
+        this.cnpj_fornecedor = cnpj_fornecedor;
+    }
+
+    public String getNome_fornecedor() {
+        return nome_fornecedor;
+    }
+
+    public void setNome_fornecedor(String nome_fornecedor) {
+        this.nome_fornecedor = nome_fornecedor;
+    }
+
+    public String getCep_fornecedor() {
+        return cep_fornecedor;
+    }
+
+    public void setCep_fornecedor(String cep_fornecedor) {
+        this.cep_fornecedor = cep_fornecedor;
+    }
+
+    public String getEmail_fornecedor() {
+        return email_fornecedor;
+    }
+
+    public void setEmail_fornecedor(String email_fornecedor) {
+        this.email_fornecedor = email_fornecedor;
+    }
+
+}
